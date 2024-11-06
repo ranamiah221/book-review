9a67d8fb7f640dc5de444f77da0ed64519adce89
ranamiah221 <ranarasul21@gmail.com>
2024-11-06 13:25:58 +0600

    added a responsive Navbar

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,8 +1,8 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 shadow-md">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -26,28 +26,76 @@ const Navbar = () => {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
             <li>
-              <NavLink to='/'>Home</NavLink>
+              <NavLink
+                to="/"
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "btn text-green-500 bg-white border-green-500" : ""
+                }
+              >
+                Home
+              </NavLink>
             </li>
             <li>
-              <NavLink to='/bookList'>Book List</NavLink>
+              <NavLink
+                to="/bookList"
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "btn text-green-500 bg-white border-green-500" : ""
+                }
+              >
+               List of Book
+              </NavLink>
             </li>
-            
+            <li>
+              <NavLink
+                to="/pageOfRead"
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "btn text-green-500 bg-white border-green-500" : ""
+                }
+              >
+                Page of Read
+              </NavLink>
+            </li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <h2 className="text-xl font-bold ">Book Vibe</h2>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
         <li>
-              <NavLink to='/'>Home</NavLink>
+              <NavLink
+                to="/"
+                className={({ isActive, isPending }) =>
+                  isPending ? "" : isActive ? "btn text-green-500 bg-white border-green-500 -mt-2 hover:bg-white mx-2" : ""
+                }
+              >
+                Home
+              </NavLink>
             </li>
             <li>
-              <NavLink to='/bookList'>Book List</NavLink>
+              <NavLink
+                to="/bookList"
+                className={({ isActive, isPending }) =>
+                  isPending ? "" : isActive ? "btn text-green-500 bg-white border-green-500 -mt-2 hover:bg-white mx-2" : ""
+                }
+              >
+               List of Book
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/pageOfRead"
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "btn text-green-500 bg-white border-green-500 -mt-2 hover:bg-white mx-2" : ""
+                }
+              >
+                Page of Read
+              </NavLink>
             </li>
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <Link><button className="bg-green-400 text-white px-3 py-2 rounded-lg font-medium mr-2">Sign in</button></Link>
+        <Link><button className="bg-cyan-500 text-white px-3 py-2 rounded-lg font-medium ">Sign up</button></Link>
       </div>
     </div>
   );
