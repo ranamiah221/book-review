22ce1a3d4c866cffee261b398e13d57305390166
ranamiah221 <ranarasul21@gmail.com>
2024-11-09 00:36:16 +0600

    added a react recharts in read of page

--- src/pages/pageOfRead/PageOfRead.jsx
+++ src/pages/pageOfRead/PageOfRead.jsx
@@ -1,11 +1,71 @@
-import React from 'react';
+import React from "react";
+import { BarChart, CartesianGrid } from "recharts";
+import { getReadItem } from "./../../index";
+import { Bar, Cell, XAxis, YAxis } from "recharts";
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
 
 const PageOfRead = () => {
-    return (
-        <div>
-            <h1>hello read page</h1>
-        </div>
-    );
+  const readBook = getReadItem();
+  console.log(readBook);
+
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+  ];
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${
+      x + width / 2
+    },${y + height / 3}
+      ${x + width / 2}, ${y}
+      C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${
+      y + height
+    } ${x + width}, ${y + height}
+      Z`;
+  };
+
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
+
+  return (
+    <div className="flex justify-center items-center bg-[#1313130C] p-10">
+      <div>
+        <BarChart
+          width={1000}
+          height={400}
+          data={readBook}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="bookName" />
+          <YAxis />
+          <Bar
+            dataKey="totalPages"
+            fill="#8884d8"
+            shape={<TriangleBar />}
+            label={{ position: "top" }}
+          >
+            {
+            readBook.map((book, index) => (
+              <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+              
+            ))}
+          </Bar>
+        </BarChart>
+      </div>
+    </div>
+  );
 };
 
 export default PageOfRead;
